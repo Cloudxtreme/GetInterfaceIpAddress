b3ba89fc9ac999b6dea242f0d4790267781a9ba9
vvboborykin <bpost@yandex.ru>
2015-09-15 11:10:33 +0300

    Initial publication

---
+++ GetInterfaceIpAddress/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetInterfaceIpAddress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var interfaceName = args.Length > 0 ? args[0] : "VPN";
+            foreach (NetworkInterface ni in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (ni.Name == interfaceName)
+                {
+                    foreach (UnicastIPAddressInformation ip in ni.GetIPProperties().UnicastAddresses)
+                    {
+                        if (ip.Address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                        {
+                            Console.WriteLine(ip.Address.ToString());
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
